eabd594b43351d7a65ed299e37eb2fe999e9ba73
rishabhpillay <rishabhpillay03@gmail.com>
2024-10-24 15:43:21 +0530

    add console for debugging

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -3,12 +3,16 @@ const jwt = require('jsonwebtoken');
 const setUserInReqFromCookie = (req, res, next) => {
   try {
     const cookies = req.headers?.cookie?.split('; ');
+    console.log(`cookies: ${cookies}`);
     if(!cookies) return res.status(401).send('UNAUTHORIZED');
+    
     for(let secret of cookies){
         if(secret.includes('token=')){
           const token = secret.split('token=')[1];
           const decoded = jwt.verify(token, process.env.JWT_SECRET || 'JWT_SECRET');
           if(!decoded) return res.status(401).send('UNAUTHORIZED');
+          console.log(decoded);
+                                                  
           req.decodedUser = decoded.user;
           next();
           return;
