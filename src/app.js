d70d2f178520bd80a96f2619c90f9f4ad8fbb7f2
yashpriyam <52121893+yashpriyam@users.noreply.github.com>
2024-11-22 13:10:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,10 +17,7 @@ connectDB();
 
 
 
-app.use(cors({
-    origin: ["https://erp-pwa.netlify.app", "http://localhost:3000", "https://main--erp-pwa.netlify.app", "http://192.168.29.198:3000"],
-    credentials: true
-}))
+app.use(cors())
 // Middleware to parse JSON
 app.use(bodyparser.json());
 app.use(fileUpload({
